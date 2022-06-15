34599bc80d91585341a7d307028457d9ea6547ca
Ghazwan <ghazwan.sihamudin@gmail.com>
2022-06-15 11:58:28 +0700

    feat: add storybook for button

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import AdminPage from 'pages/AdminPage';
-import ComponentDemoPage from 'pages/ComponentDemoPage';
 import PatientPage from 'pages/PatientPage';
 
 const Routers = () => {
@@ -11,7 +10,6 @@ const Routers = () => {
         {/* Root path should be <Login /> or <Register />. Below is just for example */}
         <Route path='/' element={<AdminPage />} />
         <Route path='/patient' element={<PatientPage />} />
-        <Route path='/component' element={<ComponentDemoPage />} />
 
         {/* Catch all */}
         <Route path='*' element={<h1>Not found</h1>} />
