cbf953fd866119d9f31136cc84dafc47b6451c28
Rhenald <renaldkarel@gmail.com>
2022-06-21 09:50:28 +0700

    fix name pattern import

--- src/pages/receptionist/queuemedicine/index.tsx
+++ src/pages/receptionist/queuemedicine/index.tsx
@@ -5,7 +5,7 @@ import Headers from 'components/TableData/Headers';
 import Rows from 'components/TableData/Rows';
 
 import queueMedicineData from 'utils/constants/queueMedicineData';
-import { QUEUE_TYPE_MEDICINE } from './types/queue';
+import { QUEUE_TYPE_MEDICINE } from 'pages/receptionist/queueMedicine/types/queue';
 import QueueMedicineData from 'types/QueueMedicineData';
 
 const QueueMedicine = () => {
