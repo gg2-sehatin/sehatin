59c541b834894ca6ea95e04239fd3899a2dda79f
medialisar <medialisar99@gmail.com>
2022-06-23 21:47:59 +0700

    stats card

--- src/pages/administration/index.tsx
+++ src/pages/administration/index.tsx
@@ -1,45 +1,10 @@
 import {
   Box,
   chakra,
-  Flex,
   SimpleGrid,
-  Stat,
-  StatLabel,
-  StatNumber,
-  useColorModeValue,
 } from '@chakra-ui/react';
 import { BsPerson } from 'react-icons/bs';
-import { StatsCardProps } from './types';
-
-function StatsCard(props: StatsCardProps) {
-  const { title, stat, icon } = props;
-  return (
-    <Stat
-      px={{ base: 2, md: 4 }}
-      py={'5'}
-      shadow={'xl'}
-      border={'1px solid'}
-      borderColor={useColorModeValue('gray.800', 'gray.500')}
-      rounded={'lg'}>
-      <Flex justifyContent={'space-between'}>
-        <Box pl={{ base: 2, md: 4 }}>
-          <StatLabel fontWeight={'medium'}>
-            {title}
-          </StatLabel>
-          <StatNumber fontSize={'2xl'} fontWeight={'medium'}>
-            {stat}
-          </StatNumber>
-        </Box>
-        <Box
-          my={'auto'}
-          color={useColorModeValue('gray.800', 'gray.200')}
-          alignContent={'center'}>
-          {icon}
-        </Box>
-      </Flex>
-    </Stat>
-  );
-}
+import  StatsCard  from 'components/atoms/StatsCard';
 
 export default function Administration() {
   return (
