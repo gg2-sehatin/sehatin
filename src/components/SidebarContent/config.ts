b68de25ccb8aac48357a08f95ecb177aec2ada1b
unknown <syarifarhy23@gmail.com>
2022-07-01 20:15:25 +0700

    create logo-icon

--- src/components/SidebarContent/config.ts
+++ src/components/SidebarContent/config.ts
@@ -1,27 +1,36 @@
-import { FiHome, FiBook } from "react-icons/fi";
+import {
+  FiHome,
+  FiBook,
+  FiFilePlus,
+  FiCalendar,
+  FiUsers,
+  FiList,
+  FiClock,
+  FiDollarSign,
+} from "react-icons/fi";
 
 export const DoctorLinks = [
   { name: "Beranda", icon: FiHome, href: "/" },
   { name: "Daftar EMR", icon: FiBook, href: "/emr-history" },
-  { name: "Tambah EMR", icon: FiHome, href: "/add-emr" },
+  { name: "Tambah EMR", icon: FiFilePlus, href: "/add-emr" },
 ];
 
 export const PatientLinks = [
   { name: "Beranda", icon: FiHome, href: "/" },
-  { name: "Form Reservasi", icon: FiHome, href: "/form-reservasi" },
-  { name: "Riwayat EMR", icon: FiHome, href: "/my-emr" },
+  { name: "Form Reservasi", icon: FiCalendar, href: "/form-reservasi" },
+  { name: "Riwayat EMR", icon: FiBook, href: "/my-emr" },
 ];
 
 export const AdminLinks = [
   { name: "Beranda", icon: FiHome, href: "/" },
-  { name: "Obat", icon: FiHome, href: "/medicine" },
-  { name: "Jadwal", icon: FiHome, href: "/schedule" },
-  { name: "Pengguna Aplikasi", icon: FiHome, href: "/user" },
+  { name: "Obat", icon: FiList, href: "/medicine" },
+  { name: "Jadwal", icon: FiClock, href: "/schedule" },
+  { name: "Pengguna Aplikasi", icon: FiUsers, href: "/user" },
 ];
 
 export const ReceptionistLinks = [
   { name: "Beranda", icon: FiHome, href: "/" },
-  { name: "Antrian Pasien", icon: FiHome, href: "/queue-patient" },
-  { name: "Antrian Obat", icon: FiHome, href: "/queue-medicine" },
-  { name: "Pembayaran", icon: FiHome, href: "/billing-payment" },
+  { name: "Antrian Pasien", icon: FiUsers, href: "/queue-patient" },
+  { name: "Antrian Obat", icon: FiUsers, href: "/queue-medicine" },
+  { name: "Pembayaran", icon: FiDollarSign, href: "/billing-payment" },
 ];
